a4969a88e6dc0bb075fb44094ca18640668e41fd
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-09-13 10:36:53 -0600

    Add header element to ArticleCard and add css styling

---
+++ src/Components/ArticleCard/ArticleCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ArticleCard.css'
+
+const ArticleCard = () => {
+	return (
+		<div className="article-card">
+			<h2>I'm an Article Card</h2>
+		</div>
+	)
+}
+
+export default ArticleCard
