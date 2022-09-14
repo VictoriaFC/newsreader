b218518acf3a901a35fe6f1556e18cce28a2ba78
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-09-13 19:39:25 -0600

    Move env file to root directory, add env to .gitignore file and update api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,4 @@
 export const fetchArticlesBySection = (section) => {
-  return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key={process.env.REACT_APP_API_KEY}`)
+	return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${process.env.REACT_APP_API_KEY}`)
   .then(response => response.json())
 }
