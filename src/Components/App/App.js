ad762c0c63770e393e6825b27901f52d0802e1b3
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-09-14 21:22:32 -0600

    Add Route for ArticleDetails page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import './App.css';
 import FormSelect from '../FormSelect/FormSelect'
-import ArticleCard from '../ArticleCard/ArticleCard';
 import Articles from '../Articles/Articles';
 import ArticleDetails from '../ArticleDetails/ArticleDetails'
 
@@ -13,16 +12,23 @@ const App = () => {
 
   return (
     <div className="main-container">
+
 			<Route exact path="/">
 				<FormSelect setArticles={setArticles} setError={setError} error={error}/>
 				<Articles setArticles={setArticles} articles={articles}/>
-				<ArticleCard />
-				<ArticleCard />
-				<ArticleCard />
 			</Route>
-			{/* <Route exact path="/Article/:id">
-				<ArticleDetails />
-			</Route> */}
+
+			<Route exact path="/Article/:id"
+				render={({ match }) => {
+					const articleToRender = articles.find(article => article.created_date === match.params.id)
+					return (
+					<div>
+						<ArticleDetails {...articleToRender}/>
+					</div>
+					)
+				}}
+			/>
+
     </div>
   )
 }
