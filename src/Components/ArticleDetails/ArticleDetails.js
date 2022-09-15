7c0c68e8a63fb5f76b7e54f0e7110241bd7d7c3e
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-09-15 12:02:03 -0600

    Remove time from published date

--- src/Components/ArticleDetails/ArticleDetails.js
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -4,12 +4,18 @@ import './ArticleDetails.css'
 
 const ArticleDetails = ({ title, byline, abstract, published_date }) => {
 
+	const newDate = (published_date) => {
+		return published_date.split('T')[0]
+	}
+
+
+
 	return (
 		<div className="article-details-container">
 			<h2 className="details-title">{title}</h2>
 			<p className="details-byline">{byline}</p>
 			<p className="details-abstract">Description: {abstract}</p>
-			<p className="details-pub-date">{published_date}</p>
+			<p className="details-pub-date">Published: {newDate(published_date)}</p>
 			<Link to="/">
 				<button className="details-back-button">Back to Main</button>
 			</Link>
