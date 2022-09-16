f97126144bcd95ac8541cfc3a057bbf12650cf59
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-09-15 21:05:55 -0600

    Add additional styling and more details

--- src/Components/ArticleDetails/ArticleDetails.js
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './ArticleDetails.css'
 
-const ArticleDetails = ({ title, byline, abstract, published_date }) => {
+const ArticleDetails = ({ title, byline, abstract, published_date, multimedia, short_url }) => {
 
 	const newDate = (published_date) => {
 		return published_date.split('T')[0]
@@ -11,14 +11,18 @@ const ArticleDetails = ({ title, byline, abstract, published_date }) => {
 
 
 	return (
-		<div className="article-details-container">
-			<h2 className="details-title">{title}</h2>
-			<p className="details-byline">{byline}</p>
-			<p className="details-abstract">Description: {abstract}</p>
-			<p className="details-pub-date">Published: {newDate(published_date)}</p>
-			<Link to="/">
-				<button className="details-back-button">Back to Main</button>
-			</Link>
+		<div className="article-details-wrapper">
+			<div className="article-details-container">
+				<h2 className="details-title">{title}</h2>
+				<p className="details-byline">{byline}</p>
+				<img className="details-image"src={multimedia[0].url} alt="article-image"/>
+				<p className="details-abstract">Description: <br></br> {abstract}</p>
+				<p className="details-pub-date">Published: {newDate(published_date)}</p>
+				<a href={short_url} className="nyt-button" target="_blank" rel="noopener noreferrer">See Article on New York Times</a>
+				<Link to="/">
+					<button className="details-back-button">Back to Main</button>
+				</Link>
+			</div>
 		</div>
 	)
 }
